829d60d6afbd0076e3d6e9680a25b20aa703a3ed
geraldon1997 <geraldon1997@gmail.com>
2022-11-23 19:09:59 +0100

    working on mobile view

--- src/components/Restmode.js
+++ src/components/Restmode.js
@@ -5,25 +5,25 @@ import "./Restmode.css";
 
 function Restmode() {
     return (
-        <section className="restmode" loading="lazy">
+        <section className="restmode">
             <h2 className="title">Restmodes</h2>
             <div className="group--list">
                 <div className="group--box group--box--btn--bg">
-                    <img src={phoneLeft} alt="..." className="group--box--image" />
+                    <img src={phoneLeft} alt="..." className="group--box--image" loading="lazy" />
                     <div className="group--box--text">
                         <div className="group--box--caption">Rest - (Rest Phase)</div>
                         <div className="group--box--description">Put your phone in Sleep to earn $EASE</div>
                     </div>
                 </div>
                 <div className="group--box group--box--btn--bg">
-                    <img src={phoneMiddle} alt="..." className="group--box--image group--box--image--middle" />
+                    <img src={phoneMiddle} alt="..." className="group--box--image group--box--image--middle" loading="lazy" />
                     <div className="group--box--text">
                         <div className="group--box--caption">Drive - (Vehicle Phase)</div>
                         <div className="group--box--description">Put your phone down while Driving to earn $EASE</div>
                     </div>
                 </div>
                 <div className="group--box group--box--btn--bg">
-                    <img src={phoneRight} alt="..." className="group--box--image group--box--image--right" />
+                    <img src={phoneRight} alt="..." className="group--box--image group--box--image--right" loading="lazy" />
                     <div className="group--box--text">
                         <div className="group--box--caption">Mental - (Rest Phase)</div>
                         <div className="group--box--description">Put your phone in during the day to earn $EASE</div>
