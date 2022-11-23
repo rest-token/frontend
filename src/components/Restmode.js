855871d4e225d06fd45c67fca9c42c123cb0d394
geraldon1997 <geraldon1997@gmail.com>
2022-11-23 14:14:32 +0100

    working on restmode section

--- src/components/Restmode.js
+++ src/components/Restmode.js
@@ -1,5 +1,7 @@
 import phoneRight from "../images/3d-smartphone-with-protection-lock-code-password-correct-mark-3d-render-with-clippding-path 1.png";
 import phoneLeft from "../images/3d-smartphone-with-protection-lock-code-password-correct-mark-3d-render-with-clipping-path 1.png";
+import phoneMiddle from "../images/3d-smartphone-with-protection-lock-code-password-correct-mark-3d-render-with-clipping-path2 1.png";
+import "./Restmode.css";
 
 function Restmode() {
     return (
@@ -8,10 +10,25 @@ function Restmode() {
             <div className="group--list">
                 <div className="group--box group--box--btn--bg">
                     <img src={phoneLeft} alt="..." className="group--box--image" />
-                    <div className="group--box--caption">Rest - (Rest Phase)</div>
+                    <div className="group--box--text">
+                        <div className="group--box--caption">Rest - (Rest Phase)</div>
+                        <div className="group--box--description">Put your phone in Sleep to earn $EASE</div>
+                    </div>
                 </div>
-                <div className="group--box group--box--btn--bg"></div>
-                <div className="group--box group--box--btn--bg"></div>
+                <div className="group--box group--box--btn--bg">
+                    <img src={phoneMiddle} alt="..." className="group--box--image group--box--image--middle" />
+                    <div className="group--box--text">
+                        <div className="group--box--caption">Drive - (Vehicle Phase)</div>
+                        <div className="group--box--description">Put your phone down while Driving to earn $EASE</div>
+                    </div>
+                </div>
+                <div className="group--box group--box--btn--bg">
+                    <img src={phoneRight} alt="..." className="group--box--image group--box--image--right" />
+                    <div className="group--box--text">
+                        <div className="group--box--caption">Mental - (Rest Phase)</div>
+                        <div className="group--box--description">Put your phone in during the day to earn $EASE</div>
+                    </div>
+                </div>
             </div>
         </section>
     )
