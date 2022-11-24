92bc664fa4105728a11154aaaebacbc1c2221f3e
geraldon1997 <geraldon1997@gmail.com>
2022-11-24 22:47:19 +0100

    made the navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,37 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../images/logo.png";
+import menu from "../images/menu.png";
+
+function myFunction() {
+    var x = document.getElementById("nav--menu");
+        if (x.className === "nav--menu") {
+            x.classList.add('responsive')
+            x.style.position = 'absolute';
+            x.style.display = 'flex';
+            x.style.flexDirection = 'column';
+            x.style.left = '33%'
+        } else {
+            x.classList.remove('responsive')
+            x.style.display = 'none';
+        }
+}
+
+function closeMenu() {
+    var menu = document.getElementById("nav--menu");
+    var children = menu.childNodes;
+    
+    for (let i = 0; i < children.length; i++) {
+        children[i].addEventListener('click', function () {
+            menu.style.display = 'none';
+            menu.classList.remove('responsive');
+        });
+    }
+}
+
+setTimeout(() => {
+    closeMenu()
+}, 2000);
 
 function Navbar(props) {
     return (
@@ -8,10 +39,11 @@ function Navbar(props) {
             <Link to={props.url.pages.home}>
                 <img src={logo} className="nav--logo" alt="logo" />
             </Link>
-            <div className="nav--menu">
+            <div className="nav--menu" id="nav--menu">
                 <Link to={props.url.pages.sale} className="btn btn--default">Pre-Sale</Link>
                 <a href={ props.url.docs } rel="noreferrer" target="_blank" className="btn btn--default" >Documentation</a>
             </div>
+            <img src={menu} alt="..." className="icon" id="menu--icon" onClick={myFunction} />
         </nav>
     )
 }
