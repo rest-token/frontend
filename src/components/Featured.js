6100245a461efad344504b8dd8adfe0626c580a2
geraldon1997 <geraldon1997@gmail.com>
2022-11-25 11:41:56 +0100

    started work on Featured section

---
+++ src/components/Featured.js
@@ -0,0 +1,11 @@
+import "./Featured.css";
+
+function Featured(){
+    return (
+        <section className="featured">
+            <h2 className="title">As Featured In</h2>
+        </section>
+    )
+}
+
+export default Featured;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Restmode from "../components/Restmode";
 import Roadmap from "../components/Roadmap";
 import DownloadApp from "../components/DownloadApp";
+import Featured from "../components/Featured";
 
 function Home(props) {
     return (
@@ -12,6 +13,7 @@ function Home(props) {
             <AfterRestMode />
             <Roadmap />
             <DownloadApp />
+            <Featured />
         </>
     )
 }
