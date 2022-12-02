67ac372a361cf9e2ba5ff80f196725bf2cac2b7e
geraldon1997 <geraldon1997@gmail.com>
2022-12-02 22:36:21 +0100

    added more content to the home page

--- src/components/home/Partners.js
+++ src/components/home/Partners.js
@@ -1,14 +1,22 @@
 import "./Partners.css";
+import reactLogo from "../../images/logo512.png";
+import mellowCapitals from "../../images/mellow-capitals.png";
+import cloudflare from "../../images/cloudflare.png";
 
 function Partners() {
     return (
         <section className="partners">
-            <h2 className="title">Partners</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a sagittis velit, sed mattis eros. Vestibulum eu lobortis libero. Quisque faucibus nibh sapien, id ultricies ligula mollis sagittis. Cras ultrices congue turpis. Donec</p>
+            <h2 className="title">Partners And Integrations</h2>
             <div className="group--list">
-                <div className="group--box group--box--light"></div>
-                <div className="group--box group--box--light"></div>
-                <div className="group--box group--box--light"></div>
+                <div className="group--box group--box--light">
+                    <img src={mellowCapitals} alt="..." />
+                </div>
+                <div className="group--box group--box--btn--bg">
+                    <img src={reactLogo} alt="..." />
+                </div>
+                <div className="group--box group--box--light cloudflare">
+                    <img src={cloudflare} alt="..." />
+                </div>
             </div>
         </section>
     )
