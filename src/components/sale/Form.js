072d1ca93deed5e953863af0c5f4e24cf1fd2322
geraldon1997 <geraldon1997@gmail.com>
2022-12-02 21:57:38 +0100

    finished the sale page form

--- src/components/sale/Form.js
+++ src/components/sale/Form.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./Form.css";
 import { FaCopy } from "react-icons/fa";
-import CopyToClipboard from "react-copy-to-clipboard";
+import cardanoAsset from "../../images/cardano 1@4x 1.png";
+import restHalfLogo from "../../images/rest logo 1.png";
 
 function Form(){
     const sale_price = 500;
@@ -9,7 +10,7 @@ function Form(){
     const max = 5000;
     const [cardano, setCardano] = useState(0);
     const [rest, setRest] = useState("Amount of $REST you will receive");
-    const [address, setAddress] = useState("address");
+    const address = "address";
 
     function handleInput(event) {
         let cardano_amount = (event.target.value).trim();
@@ -21,20 +22,50 @@ function Form(){
             setRest("Amount of $REST you will receive");
         }
     }
+
+    function copyAddress(event) {
+        var textField = document.createElement('textarea')
+        textField.innerText = address
+        document.body.appendChild(textField)
+        textField.select()
+        document.execCommand('copy')
+        textField.remove()
+
+        const button = event.target;
+        button.innerText = "Copied !";
+
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+    }
+
     return (
         <section className="form">
 
             <div className="address--box">
                 <input type="text" id="address" value={address} readOnly />
-                <button type="button" class="btn btn--default" id="copy-address" onClick={navigator.clipboard.writeText(address)}><FaCopy /> Copy</button>
+                <button type="button" className="btn btn--default" id="copy-address" onClick={copyAddress}><FaCopy /> Copy</button>
             </div>
             
-            <input type="number" placeholder="Enter CARDANO Amount" min={min} max={max} value={cardano} onChange={handleInput} />
+            <div className="cardano--amount--box">
+                <div className="cardano--logo">
+                    <img src={cardanoAsset} alt="..." />
+                </div>
+                <input type="number" placeholder="Enter CARDANO Amount" min={min} max={max} value={cardano} onChange={handleInput} />
+            </div>
+            
             <div className="polygon--box">
                 <div className="polygon--up"></div>
                 <div className="polygon--down"></div>
             </div>
-            <input type="text" value={rest} placeholder="Amount of $REST you will receive" readOnly />
+
+            <div className="rest--amount--box">
+                <div className="rest--logo">
+                    <img src={restHalfLogo} alt="..." />
+                </div>
+                <input type="text" value={rest} placeholder="Amount of $REST you will receive" readOnly />
+            </div>
+            
         </section>
     )
 }
