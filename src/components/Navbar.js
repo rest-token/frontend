009117d423c3ccc9ad7b9ef323b92b553caf67ea
geraldon1997 <geraldon1997@gmail.com>
2022-12-02 12:52:34 +0100

    changed the menu display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../images/logo.png";
 import menu from "../images/menu.png";
 import $ from "jquery";
 
 function Navbar(props) {
+    const location = useLocation();
+    const route = location.pathname;
 
     function openCloseMenu() {
         var x = document.getElementById("nav--menu");
@@ -13,8 +15,9 @@ function Navbar(props) {
             x.style.position = 'absolute';
             x.style.display = 'flex';
             x.style.flexDirection = 'column';
-            x.style.gap = '5px';
+            x.style.gap = '10px';
             x.style.left = '33%';
+            x.style.zIndex = '999';
         } else {
             x.classList.remove('responsive')
             x.style.display = 'none';
@@ -37,10 +40,10 @@ function Navbar(props) {
                 <img src={logo} className="nav--logo" alt="logo" />
             </Link>
             <div className="nav--menu" id="nav--menu">
-                <Link to={props.url.pages.home} className="btn btn--default menu--link" onClick={checkBodyClickForMenu}>Home</Link>
-                <a href={props.url.pitchdeck} className="btn btn--default menu--link" onClick={checkBodyClickForMenu}>Pitchdeck</a>
-                <Link to={props.url.pages.sale} className="btn btn--default menu--link" onClick={checkBodyClickForMenu}>Pre-Sale</Link>
-                <a href={ props.url.docs } rel="noreferrer" target="_blank" className="btn btn--default menu--link" onClick={checkBodyClickForMenu}>Documentation</a>
+                <Link to={props.url.pages.home} className="menu--link" onClick={checkBodyClickForMenu}>Home</Link>
+                <a href={props.url.pitchdeck} className="menu--link" onClick={checkBodyClickForMenu}>Pitchdeck</a>
+                { route !== '/sale' && <Link to={props.url.pages.sale} className="menu--link" onClick={checkBodyClickForMenu}>Pre-Sale</Link>}
+                <a href={ props.url.docs } rel="noreferrer" target="_blank" className="menu--link" onClick={checkBodyClickForMenu}>Documentation</a>
             </div>
             <img src={menu} alt="..." className="icon" id="menu--icon" onClick={handleMenu} />
         </nav>
