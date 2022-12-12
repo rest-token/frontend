60bd258af5e59d00ac575482fb36d336f84b0cc8
geraldon1997 <geraldon1997@gmail.com>
2022-12-12 05:12:01 +0100

    added pitchdeck

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -9,7 +9,7 @@ function Hero(props) {
             <div className="hero--left">
                 <img src={restEase} alt="hero" className="hero--image" />
                 <p className="hero--text">REST, a Social-fi and a Game-fi infused Web3 lifestyle app that rewards users when they switch to DND mode on their phones.</p>
-                <Link to={props.url.pitchDeck} className="btn btn--default">Pitch Deck</Link>
+                <a href={props.url.pitchDeck} rel="noreferrer" target="_blank" className="btn btn--default">Pitch Deck</a>
             </div>
             <div className="hero--right">
                 <img src={restVideo} alt="..." className="hero--video" />
