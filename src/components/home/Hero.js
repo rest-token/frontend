6b6e28c2cebde03f95ef8cd79d728870143c6360
geraldon1997 <geraldon1997@gmail.com>
2022-12-12 05:08:15 +0100

    added hover effect for a tag

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -9,7 +9,7 @@ function Hero(props) {
             <div className="hero--left">
                 <img src={restEase} alt="hero" className="hero--image" />
                 <p className="hero--text">REST, a Social-fi and a Game-fi infused Web3 lifestyle app that rewards users when they switch to DND mode on their phones.</p>
-                <Link to={props.url.pages.pitchDeck} className="btn btn--default">Pitch Deck</Link>
+                <Link to={props.url.pitchDeck} className="btn btn--default">Pitch Deck</Link>
             </div>
             <div className="hero--right">
                 <img src={restVideo} alt="..." className="hero--video" />
